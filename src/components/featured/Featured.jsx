0e3136a123fc5f173849feda2c4a7f8d603ef226
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-08 02:18:04 +0700

    Style: CardList component

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -6,7 +6,7 @@ export default function Featured() {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <b>Hey, Wenny dev here!</b> Discover my stories and creative ideas.
+        <b>Hey, Wennie dev here!</b> Discover my stories and creative ideas.
       </h1>
       <div className={styles.post}>
         <div className={styles.imgContainer}>
