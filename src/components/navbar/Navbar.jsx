16df74be7188a01b4d054037f057c4502b898190
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-07 18:22:02 +0700

    Style: NavBar responsiveMenu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,12 +14,12 @@ export default function Navbar() {
         <Image src="/tiktok.png" alt="tiktok" width={24} height={24} />
         <Image src="/youtube.png" alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>wennyblog</div>
+      <div className={styles.logo}>wennyblog</div> 
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/">Homepage</Link>
-        <Link href="/">Contact</Link>
-        <Link href="/">About</Link>
+        <Link href="/" className={styles.link}>Homepage</Link>
+        <Link href="/" className={styles.link}>Contact</Link>
+        <Link href="/" className={styles.link}>About</Link>
         <AuthLinks />
       </div>
     </div>
