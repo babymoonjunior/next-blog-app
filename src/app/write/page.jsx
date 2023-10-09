606de2253ad8677f36ae5023bd3bfc542c21e5a8
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-10 00:32:04 +0700

    Feat: Add  mongoDB & Prisma Connection

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -5,16 +5,29 @@ import styles from "./writePage.module.css";
 import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
 export default function WritePage() {
   const [open, setOpen] = useState(false);
   const [value, setValue] = useState("");
 
+  const { status } = useSession();
+  const router = useRouter();
+
+  if (status === "loading") {
+    return <div className={styles.loading}>Loading...</div>;
+  }
+
+  if (status === "authenticated") {
+    router.push("/");
+  }
+
   return (
     <div className={styles.container}>
       <input type="text" placeholder="Title" className={styles.input} />
       <div className={styles.editor}>
-        <button className={styles.button} onClick={()=>setOpen(!open)}>
+        <button className={styles.button} onClick={() => setOpen(!open)}>
           <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
         {open && (
--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -3,19 +3,21 @@
 import React, { useState } from "react";
 import styles from "./authLinks.module.css";
 import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
 
 export default function AuthLinks() {
   const [open, setOpen] = useState(false);
 
-  const status = "notauthenticated";
+  const {status} = useSession();
+
   return (
     <>
-      {status === "notauthenticated" ? (
+      {status === "unauthenticated" ? (
         <Link href="/login" className={styles.link}>Login</Link>
       ) : (
         <>
           <Link href="/write" className={styles.link}>Write</Link>
-          <span className={styles.link}>Logout</span>
+          <span className={styles.link} onClick={signOut}>Logout</span>
         </>
       )}
       <div className={styles.burger} onClick={() => setOpen(!open)}>
