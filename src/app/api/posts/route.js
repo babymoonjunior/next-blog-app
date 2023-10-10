ed208ae8b4e35a8d2864ef9e0a68f45e32f9a2ff
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-10 21:21:13 +0700

    Feat: add comments api

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -5,20 +5,24 @@ export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
 
   const page = searchParams.get("page");
+  const cat = searchParams.get("cat");
 
   const POST_PER_PAGE = 2; //จำนวนโพสต์ที่ต้องการแสดงต่อ 1 หน้า
 
   const query = {
     take: POST_PER_PAGE,
     skip: POST_PER_PAGE * (page - 1),
+    where: {
+      ...(cat && { catSlug: cat }),
+    },
   };
 
   try {
     const [posts, count] = await prisma.$transaction([
       prisma.Post.findMany(query),
-      prisma.Post.count(),
+      prisma.Post.count({ where: query.where }),
     ]);
-    return new NextResponse(JSON.stringify({posts, count}, { status: 200 }));
+    return new NextResponse(JSON.stringify({ posts, count }, { status: 200 }));
   } catch (err) {
     console.log(err);
     return new NextResponse(
